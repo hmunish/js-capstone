97711a4b37100fc61e3d0f02d0cda679a85864fb
Abdourahmane Jalloh <abdoulramanediallo44@gmail.com>
2023-06-28 18:45:35 +0000

    add test for the commentsCount

--- src/tests/counters.test.js
+++ src/tests/counters.test.js
@@ -7,3 +7,31 @@ describe("Check number of movies counter", () => {
     expect(moviesCounter()).toBe(3);
   });
 });
+
+describe('commentCount', () => {
+  test('returns the correct count of comments', () => {
+    // Arrange
+    document.body.innerHTML = `
+    <div class="comments-text">Comment (1)</div>
+    <div class="comments-text">Comment (2)</div>
+    <div class="comments-text">Comment (3)</div>
+    `;
+
+    // Act
+    const count = commentsCount();
+
+    // Assert
+    expect(count).toBe(3);
+  });
+
+  test('returns 0 when no comments are present', () => {
+    // Arrange
+    document.body.innerHTML = '';
+
+    // Act
+    const count = commentsCount();
+
+    // Assert
+    expect(count).toBe(0);
+  });
+});
