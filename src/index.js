b92c64a3d777ac6a8f8fc82f13cc857934c32fa9
Munish <hmunish5@gmail.com>
2023-06-28 13:45:10 +0200

    Fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
-import "./style.css";
-import logo from "../assets/logo.png";
-import fetchItems from "./modules/fetchItems.js";
-import renderItems from "./modules/renderItems.js";
-import renderCommentPopup from "./modules/renderCommentPopup.js";
-import {
-  createComment,
-  getComments,
-  updateLikeCount,
-  getLikes
-} from "./modules/involvement.js";
+import './style.css';
+import logo from '../assets/logo.png';
+import fetchItems from './modules/fetchItems.js';
+import renderItems from './modules/renderItems.js';
+import renderCommentPopup from './modules/renderCommentPopup.js';
+// import {
+//   createComment,
+//   getComments,
+//   updateLikeCount,
+//   getLikes,
+// } from './modules/involvement.js';
 
-const appId = "MEyKHZs5GQJjgTbCoZJe";
+// const appId = "MEyKHZs5GQJjgTbCoZJe";
 
 // createComment(appId, 50, "Test", "Test Comment");
 // getComments(50);
 // updateLikeCount(50);
-getLikes()
+// getLikes();
 
-const mainLogo = document.querySelector(".logo");
-const article = document.querySelector(".article");
+const mainLogo = document.querySelector('.logo');
+const article = document.querySelector('.article');
 
 let episodesData;
 
 // Setting logo
-mainLogo.setAttribute("src", logo);
+mainLogo.setAttribute('src', logo);
 
 // Event listeners
-window.addEventListener("DOMContentLoaded", async () => {
+window.addEventListener('DOMContentLoaded', async () => {
   episodesData = await fetchItems();
   renderItems(article, episodesData);
 });
 
 // Click event on article
-article.addEventListener("click", (e) => {
-  if (e.target.classList.contains("likeIcon")) {
-    e.target.classList.toggle("fa-heart-o");
-    e.target.classList.toggle("fa-heart");
+article.addEventListener('click', (e) => {
+  if (e.target.classList.contains('likeIcon')) {
+    e.target.classList.toggle('fa-heart-o');
+    e.target.classList.toggle('fa-heart');
     // Like feature will be added here
-  } else if (e.target.classList.contains("comments")) {
-    const dataNo = e.target.parentElement.getAttribute("data-itemno");
+  } else if (e.target.classList.contains('comments')) {
+    const dataNo = e.target.parentElement.getAttribute('data-itemno');
     renderCommentPopup(episodesData[dataNo]);
   }
 });
